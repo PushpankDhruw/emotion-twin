ea4273e61078ed8204f1b134db2461478f2be832
Pushpank Dhruw <PushpankDhruw@Gmail.Com>
2022-06-13 19:43:47 +0530

    fix some config file ⚡

---
+++ next.config.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	// * Demo mode:
+	/* reactStrictMode: true,
+	images: {
+	  domains: ['cdn.sanity.io'],
+	  loader: 'custom',
+	}, */
+  }
+  
+  module.exports = nextConfig
+  
